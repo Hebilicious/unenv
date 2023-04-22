0fc24b0addb75b85dd155927416e68a43903107c
Hebilicious <xsh4k3@gmail.com>
2023-04-23 06:09:37 +0700

    Update inherits.ts
    Fix bug that can happen when superCtor is empty

--- src/runtime/npm/inherits.ts
+++ src/runtime/npm/inherits.ts
@@ -4,13 +4,9 @@ export default function inherits(ctor: any, superCtor: any) {
   if (!superCtor) {
     return;
   }
-  ctor.super_ = superCtor;
-  ctor.prototype = Object.create(superCtor.prototype, {
-    constructor: {
-      value: ctor,
-      enumerable: false,
-      writable: true,
-      configurable: true,
-    },
-  });
+		    ctor.super_ = superCtor;
+		    var TempCtor = function () {};
+		    TempCtor.prototype = superCtor.prototype;
+		    ctor.prototype = new TempCtor();
+		    ctor.prototype.constructor = ctor;
 }
